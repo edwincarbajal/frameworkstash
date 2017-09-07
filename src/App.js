be28ef4770c808d39cd16abb25527ef2c0c09b96
Ted-Koomen <koomen.ted@gmail.com>
2017-09-07 16:46:28 -0400

    add react-router v3, create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 // import './App.css';
+import { Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return (
-    <div />
-    );
+    return <div />;
   }
 }
 
