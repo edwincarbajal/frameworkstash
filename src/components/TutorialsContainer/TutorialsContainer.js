48f18304be1e3b5d2b9f584e128fcd4430c4e510
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-12 23:13:50 -0400

    Style tutorials

--- src/components/TutorialsContainer/TutorialsContainer.js
+++ src/components/TutorialsContainer/TutorialsContainer.js
@@ -38,49 +38,50 @@ class TutorialsContainer extends Component {
     const frameworkTitle = frameworkArray[0][0];
 
     return (
-      <div className="row">
-        <div className="col-12">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-              <Link to="/">Collections</Link>
-            </li>
-            <li className="breadcrumb-item active">{frameworkTitle}</li>
-          </ol>
-        </div>
-        <div className="tutorial-framework-col col-12">
-          <div className="card">
-            <div className="card-body">
-              <h4 className="card-title">{frameworkTitle}</h4>
-              <p className="framework-description card-text">
-                This is the React Description.
-              </p>
+      <div>
+        <div className="row">
+          <div className="col-12">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link to="/">Collections</Link>
+              </li>
+              <li className="breadcrumb-item active">{frameworkTitle}</li>
+            </ol>
+          </div>
+          <div className="tutorial-framework-col col-12">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">{frameworkTitle}</h4>
+                <p className="framework-description card-text">
+                  This is the React Description.
+                </p>
+              </div>
             </div>
           </div>
+          <div className="col-12">
+            <ul className="title-headers list-inline">
+              <li className="font-weight-bold list-inline-item">
+                <h6>Tutorial Title</h6>
+              </li>
+              <li className="font-weight-bold list-inline-item float-right">
+                <h6>Date</h6>
+              </li>
+            </ul>
+          </div>
+          {this.state.tutorials.map(tutorial => {
+            return (
+              <Tutorial
+                key={tutorial.id}
+                title={tutorial.title}
+                description={tutorial.description}
+                author={tutorial.author}
+                url={tutorial.url}
+                likes={1234}
+                date={moment(tutorial.date).format('dddd, MMMM Do YYYY')}
+              />
+            );
+          })}
         </div>
-        <div className="col-12">
-          <ul className="title-headers list-inline">
-            <li className="font-weight-bold list-inline-item">
-              <h6>Tutorial Title</h6>
-            </li>
-            <li className="font-weight-bold list-inline-item float-right">
-              <h6>Date</h6>
-            </li>
-          </ul>
-        </div>
-        {this.state.tutorials.map(tutorial => {
-          return (
-            <Tutorial
-              key={tutorial.id}
-              title={tutorial.title}
-              description={tutorial.description}
-              author={tutorial.author}
-              url={tutorial.url}
-              likes={1234}
-              date={moment(tutorial.date).format('dddd, MMMM Do YYYY')}
-            />
-          );
-        })}
-
         <SubscriptionForm />
       </div>
     );
