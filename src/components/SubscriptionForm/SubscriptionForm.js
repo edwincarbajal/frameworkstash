ddc94160b037752adae88dc69e89327c267bf12a
Ted-Koomen <koomen.ted@gmail.com>
2017-09-13 17:29:15 -0400

    add post for subscription, error handling, and change type to email

--- src/components/SubscriptionForm/SubscriptionForm.js
+++ src/components/SubscriptionForm/SubscriptionForm.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './SubscriptionForm.css';
 
 class SubscriptionForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValue: ''
+    };
+  }
+
+  handleChange = event => {
+    console.log(event.target.value);
+    this.setState({ formValue: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    axios
+      .post('http://localhost:3001/v1/subscribe', {
+        email: this.state.formValue
+      })
+      .then(response => {
+        alert('Thank you for subscribing!');
+        this.setState({ formValue: '' });
+      })
+      .catch(err => {
+        alert('Email already subscribed! Try another one!');
+        this.setState({ formValue: '' });
+      });
+  };
+
+  placeholderCheck = () => {};
   render() {
     return (
       <div className="jumbotron jumbotron-fluid text-center">
         <div className="container">
           <h3>Subscribe to get notitifed when we add new tutorials.</h3>
-          <form className="form-inline">
+          <form onSubmit={this.handleSubmit} className="form-inline">
             <div className="form-group mx-auto">
               <input
-                type="text"
+                onChange={this.handleChange}
+                type="email"
                 className="form-control mb-2 mr-sm-2 mb-sm-0"
                 id="inlineFormInputName2"
+                value={this.state.formValue}
                 placeholder="Email"
               />
 
