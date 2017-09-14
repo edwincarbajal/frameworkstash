4d1aab4f1c23c1417fcf06e4f237358028d01971
Ted-Koomen <koomen.ted@gmail.com>
2017-09-14 16:01:50 -0400

    add link to home on not found component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router';
 const NotFound = () => {
-  return <div>Not found</div>;
+  return (
+    <div className="container text-center">
+      <p>
+        Where are you going? Got lost? Click <Link to="/">here</Link> to go
+        home!
+      </p>
+    </div>
+  );
 };
 
 export default NotFound;
