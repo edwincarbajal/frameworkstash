c98a7a3d63ea357aebdba77c95e50ea188d60c3d
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-15 02:17:52 -0400

    Finish styling about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,29 +2,35 @@ import React from 'react';
 
 import './About.css';
 
+import teddy from '../../assets/teddy.jpg';
+import edwin from '../../assets/edwin.jpg';
+
 const About = () => {
   return (
     <div>
       <div className="row">
-        <div className="col-12 text-center">
+        <div className="main-header col-12 text-center">
           <h3>Our vision is simple</h3>
-          <p>A community driven directory of tutorials for developers</p>
+          <p className="lead">
+            A community driven directory of tutorials for developers
+          </p>
+          <span className="pointer" />
         </div>
       </div>
       <div className="row justify-content-center">
-        <div className="col-md-8 col-sm-12 text-center">
+        <div className="main-body col-md-8 col-sm-12 text-center">
           <h2>Why we created frameworkstash?</h2>
-          <p>
+          <p className="lead">
             As graduates of Dev Bootcamp, we realize the impact community has on
             the learning process. The goal of frameworkstash is to be the hub
             for all of those who want to learn the most cutting edge frameworks
             within a community that supports its self; gathering tutorials from
             outside resources, and curating its own.
           </p>
-          <p>
+          <p className="standout lead">
             The passion for helping others is why we created frameworkstash!
           </p>
-          <p>
+          <p className="lead">
             We are also passionate in the community having a voice. Anyone, not
             only registered users can request tutorials that they would like to
             see, or subscribe to a newsletter to be notified of the new
@@ -33,17 +39,23 @@ const About = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-12 text-center">
+        <div className="main-section col-12 bg-dark text-center">
           <h2>Technologies we used</h2>
           <div className="row">
-            <div className="col-md-3">React placeholder</div>
+            <div className="col-md-3">
+              <i className="tech-stack devicon-bootstrap-plain colored" />
+            </div>
+            <div className="col-md-3">
+              <i className="tech-stack devicon-rails-plain colored" />
+            </div>
+            <div className="col-md-3">
+              <i className="tech-stack devicon-react-original colored" />
+            </div>
             <div className="col-md-3">React Router placeholder</div>
-            <div className="col-md-3">Rails placeholder</div>
-            <div className="col-md-3">Bootstrap placeholder</div>
           </div>
         </div>
       </div>
-      <div className="row">
+      <div className="row main-footer">
         <div className="col-12 text-center">
           <h2>Meet the Team</h2>
         </div>
@@ -52,24 +64,28 @@ const About = () => {
             <div className="col-md-6">
               <div className="media">
                 <img
-                  className="d-flex mr-3"
-                  src="..."
-                  alt="Generic placeholder image"
+                  className="team-img rounded-circle img-thumbnail d-flex mr-3"
+                  src={edwin}
+                  alt=""
                 />
                 <div className="media-body">
                   <h5 className="mt-0">Edwin Carbajal</h5>
-                  Entrepreneur 🚀 | Software Engineer
+                  Front End Wizard 🚀
                   <ul className="list-inline">
                     <li className="list-inline-item">
                       <a href="https://linkedin.com/in/edwincarbajal">
-                        LinkedIn
+                        <i className="fa fa-linkedin" />
                       </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="https://github.com/edwincarbajal">Github</a>
+                      <a href="https://github.com/edwincarbajal">
+                        <i className="fa fa-github" />
+                      </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="https://twitter.com/edwinxcarbajal">Twitter</a>
+                      <a href="https://twitter.com/edwinxcarbajal">
+                        <i className="fa fa-twitter" />
+                      </a>
                     </li>
                   </ul>
                 </div>
@@ -78,9 +94,9 @@ const About = () => {
             <div className="col-md-6">
               <div className="media">
                 <img
-                  className="d-flex mr-3"
-                  src="..."
-                  alt="Generic placeholder image"
+                  className="team-img rounded-circle img-thumbnail d-flex mr-3"
+                  src={teddy}
+                  alt=""
                 />
                 <div className="media-body">
                   <h5 className="mt-0">Teddy Koomen</h5>
@@ -88,14 +104,18 @@ const About = () => {
                   <ul className="list-inline">
                     <li className="list-inline-item">
                       <a href="https://www.linkedin.com/in/theodoruskoomen/">
-                        LinkedIn
+                        <i className="fa fa-linkedin" />
                       </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="https://github.com/Ted-Koomen">Github</a>
+                      <a href="https://github.com/Ted-Koomen">
+                        <i className="fa fa-github" />
+                      </a>
                     </li>
                     <li className="list-inline-item">
-                      <a href="https://twitter.com/Ted_Koomen">Twitter</a>
+                      <a href="https://twitter.com/Ted_Koomen">
+                        <i className="fa fa-twitter" />
+                      </a>
                     </li>
                   </ul>
                 </div>
