a64c627a20513bf9cc5e402ef534ea417d40b18b
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-15 02:18:23 -0400

    Refactor app component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,17 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tutorials: []
-    };
-  }
-
-  handleClick = event => {
-    event.preventDefault();
-    localStorage.setItem(
-      'frameworkId',
-      JSON.stringify([`${event.target.innerHTML}`, `${event.target.id}`])
-    );
-  };
-
   render() {
     return (
-      <div className="container">
+      <div
+        className={
+          this.props.location.pathname === '/about' ? (
+            'container-fluid'
+          ) : (
+            'container'
+          )
+        }
+      >
         <Navbar />
         {this.props.children}
         <Footer />
