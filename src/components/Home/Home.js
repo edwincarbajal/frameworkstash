1b56ff3615eb8a2934f089eb098f4e127a822388
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-16 16:00:54 -0400

    Rename MainView component to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,30 +2,32 @@ import React, { Component } from 'react';
 
 import SubscriptionForm from '../SubscriptionForm/SubscriptionForm';
 import CollectionsContainer from '../CollectionsContainer/CollectionsContainer';
-import TrendingContainer from '../TrendingContainer/TrendingContainer';
-import FeaturedContainer from '../FeaturedContainer/FeaturedContainer';
 
-class MainView extends Component {
-  subscribedCheck = () => {
-    if (localStorage.getItem('subscribed') === 'true') {
-      return (
-        <div>
-          <TrendingContainer />
-          <FeaturedContainer />
-        </div>
-      );
-    } else {
-      return <SubscriptionForm />;
-    }
-  };
+// import TrendingContainer from '../TrendingContainer/TrendingContainer';
+// import FeaturedContainer from '../FeaturedContainer/FeaturedContainer';
+
+class Home extends Component {
+  // subscribedCheck = () => {
+  //   if (localStorage.getItem('subscribed') === 'true') {
+  //     return (
+  //       <div>
+  //         <TrendingContainer />
+  //         <FeaturedContainer />
+  //       </div>
+  //     );
+  //   } else {
+  //     return <SubscriptionForm />;
+  //   }
+  // };
 
   render() {
     return (
       <div>
-        {this.subscribedCheck()}
+        {/* {this.subscribedCheck()} */}
+        <SubscriptionForm />
         <CollectionsContainer />
       </div>
     );
   }
 }
-export default MainView;
+export default Home;
