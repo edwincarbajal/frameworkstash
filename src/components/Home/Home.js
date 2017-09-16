c4462acb34d9b538154382aab82340d26d4872e2
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-16 19:56:44 -0400

    Add featured tutorials to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SubscriptionForm from '../SubscriptionForm/SubscriptionForm';
 import CollectionsContainer from '../CollectionsContainer/CollectionsContainer';
+import Tutorial from '../Tutorial/Tutorial';
 
-// import TrendingContainer from '../TrendingContainer/TrendingContainer';
-// import FeaturedContainer from '../FeaturedContainer/FeaturedContainer';
+var moment = require('moment');
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      featuredTutorials: []
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:3001/v1/tutorials/trending')
+      .then(response => {
+        this.setState({ featuredTutorials: response.data });
+      })
+      .catch(errors => {
+        console.log(errors);
+      });
+  };
   // subscribedCheck = () => {
   //   if (localStorage.getItem('subscribed') === 'true') {
   //     return (
@@ -26,6 +44,21 @@ class Home extends Component {
         {/* {this.subscribedCheck()} */}
         <SubscriptionForm />
         <CollectionsContainer />
+        {this.state.featuredTutorials.map(tutorial => {
+          return (
+            <Tutorial
+              key={tutorial.id}
+              title={tutorial.title}
+              description={tutorial.description}
+              author={tutorial.author}
+              url={tutorial.url}
+              likes={tutorial.total_likes}
+              id={tutorial.id}
+              date={moment(tutorial.date).format('MMM. D YYYY')}
+              fetchTutorials={this.fetchTutorials}
+            />
+          );
+        })}
       </div>
     );
   }
